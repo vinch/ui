8e8ff9b65f741e7d271600cb1f1982fae39c5ae3
Dries Decramer <dries.decramer@teamleader.be>
2018-06-11 09:43:07 +0200

    Prettier Linting™

--- components/datepicker/localUtils.js
+++ components/datepicker/localUtils.js
@@ -1,11 +1,17 @@
 import { DateTime, Info } from 'luxon';
 
-export const formatDay = (day, locale = 'en') => DateTime.fromJSDate(day).setLocale(locale).toLocaleString(DateTime.DATE_HUGE);
+export const formatDay = (day, locale = 'en') =>
+  DateTime.fromJSDate(day)
+    .setLocale(locale)
+    .toLocaleString(DateTime.DATE_HUGE);
 
 export const formatMonthTitle = (date, locale = 'en') =>
-  DateTime.fromJSDate(date).setLocale(locale).toLocaleString({ month: 'long', year: 'numeric'});
+  DateTime.fromJSDate(date)
+    .setLocale(locale)
+    .toLocaleString({ month: 'long', year: 'numeric' });
 
-export const formatWeekdayShort = (dayOfWeekNumber, locale = 'en') => Info.weekdays('short', { locale })[dayOfWeekNumber];
+export const formatWeekdayShort = (dayOfWeekNumber, locale = 'en') =>
+  Info.weekdays('short', { locale })[dayOfWeekNumber];
 
 export const formatWeekdayLong = (dayOfWeekNumber, locale = 'en') => Info.weekdays('long', { locale })[dayOfWeekNumber];
 
@@ -13,9 +19,13 @@ export const getFirstDayOfWeek = (locale = 'en') => 0;
 
 export const getMonths = (locale = 'en') => Info.months('long', { locale });
 
-export const formatDate = (date, format = 'L', locale = 'en') => DateTime.fromJSDate(date).setLocale(locale).toFormat(format);
+export const formatDate = (date, format = 'L', locale = 'en') =>
+  DateTime.fromJSDate(date)
+    .setLocale(locale)
+    .toFormat(format);
 
-export const parseDate = (string, format = 'L', locale = 'en') => DateTime.fromFormat(string, format, { locale }).toJSDate();
+export const parseDate = (string, format = 'L', locale = 'en') =>
+  DateTime.fromFormat(string, format, { locale }).toJSDate();
 
 export default {
   getFirstDayOfWeek,
